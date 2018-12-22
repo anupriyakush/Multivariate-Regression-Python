f7e70b3d0baf53c751a0c17616e1574fe3c0c60a
Anupriya Kushwanshi <kushwaaa@mail.uc.edu>
2018-12-22 13:41:04 -0500

    Create Multivariate.py

---
+++ Multivariate.py
@@ -0,0 +1,12 @@
+import statsmodels.api as sm
+import xlrd
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+scale = StandardScaler()
+df = pd.read_excel('C:/Users/anupr/Documents/External Courses/Udemy - datascience/cars.xls')
+print(df.head())
+x = df[['Mileage','Cylinder','Doors']]
+y = df['Price']
+x = scale.fit_transform(x)
+est = sm.OLS(y,x).fit()  #ordinary least square
+print(est.summary())
